e596230ddedff35e38f0cd4f75200890c7e6861f
Marion <sjniesen@madisoncollege.edu>
2026-05-01 08:50:22 -0500

    removed panicked profanity added to desperately create an earlier commit from Background.gd

--- godot/Backgrounds/Background.gd
+++ godot/Backgrounds/Background.gd
@@ -5,4 +5,3 @@ extends Resource
 @export var id := "background_id"
 @export var display_name := "Display Name"
 @export var texture: Texture2D
-#Literaly just putting this here to try and salvage my work oh god oh fuck
